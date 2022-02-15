3543fccb2937429c461ab7d4ee5c69feea8fc37c
NahuelRc10 <nahueltabasso@gmail.com>
2022-02-14 21:08:26 -0300

    Middleware AdminRole and enpoint test

--- middlewares/validarJwt.js
+++ middlewares/validarJwt.js
@@ -1,7 +1,10 @@
 const jwt = require('jsonwebtoken');
-const { HTTP_UNAUTHORIZED } = require('../utils/constantes');
+const { HTTP_UNAUTHORIZED, HTTP_NOT_FOUND } = require('../utils/constantes');
+const Usuario = require('../models/usuario')
+const Rol = require('../models/rol')
 
 const validarJWT = (request, response, next) => {
+
     // Leer token
     const token = request.header('Authorization');
     if (!token) {
@@ -21,8 +24,40 @@ const validarJWT = (request, response, next) => {
             msg: 'Token Invalido!'
         });
     }
+
+}
+
+const validarAdminRole = async(request, response, next) => {
+
+    try {
+        const id = request.id;
+        const usuarioDb = await Usuario.findById(id);
+        if (!usuarioDb) {
+            return response.status(HTTP_NOT_FOUND).json({
+                ok: false,
+                msg: 'Usuario no existe'
+            });
+        }
+
+        const rol = await Rol.findById(usuarioDb.rol);
+
+        if (rol.nombreRol !== 'ROLE_ADMIN') {
+            return response.status(HTTP_UNAUTHORIZED).json({
+                ok: false,
+                msg: 'Acceso denegado'
+            });
+        }
+        next();
+    } catch (error) {
+        return response.status(HTTP_UNAUTHORIZED).json({
+            ok: false,
+            msg: 'Token Invalido!'
+        });
+    }
 }
 
 module.exports = {
-    validarJWT
+    validarJWT,
+    validarAdminRole
 }
+
